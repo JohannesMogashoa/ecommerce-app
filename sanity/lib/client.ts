b362430e4982a070dcddfd4db2b25ae74be88056
Johannes Mogashoa <jomogashoa1993@gmail.com>
2023-12-24 15:58:24 +0200

    remove secret token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,11 +1,9 @@
+import { apiVersion, dataset, projectId, useCdn } from "../env";
 import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from "../env";
-
 export const client = createClient({
-    apiVersion,
-    dataset,
-    projectId,
-    useCdn,
-    token: "skmYyQffGVg5LduyVrty3LcWl7nq4MdVzYJj0QgeDcBlwjVhgv0dQQqytHo7rnDXmiG0mUhpCQ4hn1nZngR3ndD8EAR5PX7sb6pXwur64ZNljcJtoeXLJy5eiUeDPvAQjSj8AJCjxkNG5gvbmvjr4LXIzd2wVK1Y9lGqZ9JXAYouCscsCQQh",
+	apiVersion,
+	dataset,
+	projectId,
+	useCdn,
 });
